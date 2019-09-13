cdd7e7f4515c617fb870e177a89e905ef49b4209
evanstanderwick <evanstanderwick@gmail.com>
2019-09-13 16:14:57 -0400

    Added ICommand, ISprite, IController

---
+++ 3902-ocho/IController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3902_ocho
+{
+    public enum Buttons
+    {
+        D0, D1, D2, D3, D4, RightClick, Quad1LeftClick, Quad2LeftClick, Quad3LeftClick, Quad4LeftClick
+    }
+    interface IController
+    {
+        void RegisterCommand(Buttons button, ICommand command);
+        void Update();
+
+    }
+}
---
+++ 3902-ocho/ISprite.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3902_ocho
+{
+    public interface ISprite
+    {
+        Vector2 Location { get; set; }
+
+        void Update();
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
