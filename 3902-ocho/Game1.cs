705942a12ec8bcee69f49432235c67fbeffe80bd
Dylan Doidge <doidge.11@buckeyemail.osu.edu>
2019-09-26 20:09:26 -0400

    Removed unused imports and added code reviews + reflection

--- 3902-ocho/Game1.cs
+++ 3902-ocho/Game1.cs
@@ -1,8 +1,5 @@
-﻿using _3902_ocho.Interfaces;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using System.Windows.Input;
 
 namespace _3902_ocho
 {
